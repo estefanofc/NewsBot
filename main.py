e10be7df3b7295c4b143fa34e0adc4d7ed4b906e
Estefano Felipa <estefano_fc@hotmail.com>
2020-05-26 03:18:26 -0700

    fixed login info for sender

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import redis
-
+import datetime
 
 class Scraper:
     def __init__(self, keywords):
@@ -24,8 +24,49 @@ class Scraper:
 
     def email(self):
         r = redis.Redis(host='localhost', port=6379, db=0)
-        [r.get(k) for k in r.keys()]
-        print(r.keys())
+        links = [str(r.get(k)) for k in r.keys()]
+        print(links)
+
+        # email
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+
+        fromEmail = 'tesrbotemail@gmail.com'
+        toEmail = 'estefanofc8@gmail.com'
+        password = 'peru0610'
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Link"
+        msg["From"] = fromEmail
+        msg["To"] = toEmail
+
+        html = """
+                    <h4> %s links you might find interesting today:</h4>
+
+                    %s
+                """ % (len(links), '<br/><br/>'.join(links))
+
+        mime = MIMEText(html, 'html')
+
+        msg.attach(mime)
+
+        try:
+            mail = smtplib.SMTP('smtp.gmail.com', 587)
+            mail.ehlo()
+            mail.starttls()
+            mail.login(fromEmail, password)
+            mail.sendmail(fromEmail, toEmail, msg.as_string())
+            mail.quit()
+            print('Email sent!')
+        except Exception as e:
+            print('Something went wrong... %s' % e)
+
+        # flush redis
+        r.flushdb()
 
 
-Scraper(['developer'])
+s = Scraper(['database', 'internship', 'API'])
+s.parse()
+s.store()
+if datetime.datetime.now().hour == 13:
+    s.email()
